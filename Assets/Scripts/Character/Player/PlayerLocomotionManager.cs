dbbbb8447267423f3375bc3d1b964258b8566182
ShinPurinn <cliffreynard4321@gmail.com>
2024-11-16 21:09:27 +0700

    Centralized scripts by making char/player manager scripts

--- Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
+++ Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayerLocomotionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void HandleAllMovement()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Handle movements (grounded, aerial, etc)
     }
 }
